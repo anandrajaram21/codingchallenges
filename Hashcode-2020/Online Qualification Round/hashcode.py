8a24d4f846c0bc3ab3820a426f06857c90e7d05f
Anand Rajaram <anandrajaram21@gmail.com>
2020-02-21 15:53:13 +0530

    Only need to the book signing method

--- Hashcode-2020/Online Qualification Round/hashcode.py
+++ Hashcode-2020/Online Qualification Round/hashcode.py
@@ -5,12 +5,63 @@
 4. Add the books to an array, and each time before adding, check if the book is already there in the array
 5. Calculate the sum of the scores of each book and that is the max.
 """
-import json
-# Reading the input from the file.
-with open("b_read_on.txt") as f:
+import time
+
+# def book_signing():
+# 	end_date = number_of_days - 1
+# 	max_sum = 0
+# 	for i in signed_up:
+# 		i_books = libraries[i]['books']
+# 		print(i_books)
+# 		start_date = libraries[i]['starting_day']
+# 		start_date_copy = libraries[i]['starting_day']
+# 		limit = libraries[i]['book_limit']
+# 		# while start_date <= end_date:
+# 		# 	counter = 0
+# 		# 	for i in i_books:
+# 		# 		if i not in books:
+# 		# 			books.append(i)
+# 		# 			counter += 1
+# 		# 		if counter > limit:
+# 		# 			counter = 0
+# 		# 			break
+# 		# 	start_date += 1
+# 		counter = 0
+# 		for i in i_books:
+# 			if i not in books:
+# 				books.append(i)
+# 				counter += 1
+# 			if counter > limit:
+# 				counter = 0
+# 	for i in books:
+# 		max_sum += book_scores[i]
+# 	print(max_sum)
+
+
+def signup():
+	days_taken = 0
+	# Creating a list with the library ids having highest score in sorted form
+	library_score_temp = []
+	for i in libraries:
+		library_score_temp.append((i, libraries[i]['score']))
+	library_score_temp.sort(key=lambda elem: elem[1], reverse=True)
+	library_score = list(map(lambda x: x[0], library_score_temp))
+	print("List of libraries and their scores", library_score_temp)
+	print("List of libraries with high scores", library_score)
+	for i in library_score:
+		days = libraries[i]['signup']
+		days_taken += days
+		if days_taken > number_of_days:
+			break
+		signed_up.append(i)
+		libraries[i]['starting_day'] = days_taken + 1
+	print("Libraries signed up are: ", signed_up)
+
+with open("e_so_many_books.txt") as f:
 	number_of_books, number_of_libraries, number_of_days = list(map(int, f.readline().split(" ")))
 	book_scores = {}
 	signed_up = []
+	books = []
 	scores = list(map(int, f.readline().split(" ")))
 	libraries = {}
 	for i in range(number_of_books):
@@ -22,23 +73,9 @@ with open("b_read_on.txt") as f:
 		"library_books_number": details[0],
 		"books": scores,
 		"signup": details[1],
-		"book_limit": details[2]
+		"book_limit": details[2],
+		"score": sum(list(map(lambda x: book_scores[x], scores)))
 		}
 
-def signup(libs):
-	days_taken = 0
-	for i in libs:
-		days = libs[i]['signup']
-		days_taken += days
-		if days_taken > number_of_days:
-			break
-		signed_up.append(i)
-		libraries[i]['starting_day'] = days_taken + 1
-		print(days, days_taken)
-	print(signed_up)
-	print(json.dumps(libraries))
-
-print(number_of_days)
-signup(libraries)
-for i in signed_up:
-	print(libraries[i]['starting_day'])
+	signup()
+	book_signing()
